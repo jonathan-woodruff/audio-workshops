0e701841b3ad8b752396b07df7c4a2b78da57ed4
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2024-06-25 10:10:40 -0500

    added footer and finished javascript and styling

--- index.js
+++ index.js
@@ -14,18 +14,35 @@ shakeButton.addEventListener('click',showHandshake);
 
 const workshops = document.getElementsByClassName('workshop');
 
-const startWorkshop = event => {
-    const workshop = event.currentTarget;
-    workshop.classList.replace('border-pink-50','border-pink-300');
-    for (const child of workshop.children) {
-        if (child.className.includes('workshopaudio')) {
-            child.classList.replace('hidden','flex');
-        } else if (child.className.includes('cheatsheet')) {
-            child.classList.replace('hidden', 'block');
+const displaySelected = event => {
+    const selectedWorkshop = event.currentTarget;
+    if (!selectedWorkshop.classList.contains('border-pink-300')) { //if the workshop selected is not the one that is already selected
+        //set the currently selected workshop to display as unselected
+        for (let i = 0; i < workshops.length; i++) {
+            if (workshops[i].classList.contains('border-pink-300')) {
+                workshops[i].classList.replace('border-pink-300','border-pink-50');
+                for (const child of workshops[i].children) {
+                    if (child.className.includes('workshopaudio')) {
+                        child.classList.replace('flex','hidden');
+                    } else if (child.className.includes('cheatsheet')) {
+                        child.classList.replace('block','hidden');
+                    }
+                }
+            }
+        }
+        //set the selected workshop to display as selected
+        selectedWorkshop.classList.replace('border-pink-50','border-pink-300');
+        for (const child of selectedWorkshop.children) {
+            if (child.className.includes('workshopaudio')) {
+                child.classList.replace('hidden','flex');
+            } else if (child.className.includes('cheatsheet')) {
+                child.classList.replace('hidden', 'block');
+            }
         }
     }
 };
 
 for (let i = 0; i < workshops.length; i++) {
-    workshops[i].addEventListener('click', startWorkshop);
+    workshops[i].addEventListener('click', displaySelected);
+    workshops[i].addEventListener('focus-within', displaySelected);
 };
