3b04e66dfa6c0050b78549fe5198a17225a9176b
Jonathan Woodruff <jonathanmwoodruff@gmail.com>
2024-06-26 02:13:09 -0500

    updated styling and got rid of some comments

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-/* handshake animation
-const shakeButton = document.getElementById('shake-button');
-
-const showHandshake = () => {
-    document.getElementById('handshake').style.display = 'block';
-    document.getElementById('shake-button').style.display = 'none';
-    let greeting = document.getElementById('greeting');
-    greeting.style.display = 'block';
-    greeting.style.color = 'rgb(86,84,84)';
-};
-
-shakeButton.addEventListener('click',showHandshake);
-*/
-
 const workshops = document.getElementsByClassName('workshop');
 
 const displaySelected = event => {
     const selectedWorkshop = event.currentTarget;
-    if (!selectedWorkshop.classList.contains('border-pink-300')) { //if the workshop selected is not the one that is already selected
+    if (!selectedWorkshop.classList.contains('border-amber-200')) { //if the workshop selected is not the one that is already selected
         //set the currently selected workshop to display as unselected
         for (let i = 0; i < workshops.length; i++) {
-            if (workshops[i].classList.contains('border-pink-300')) {
-                workshops[i].classList.replace('border-pink-300','border-pink-50');
+            if (workshops[i].classList.contains('border-amber-200')) {
+                workshops[i].classList.replace('border-amber-200','border-amber-50');
                 for (const child of workshops[i].children) {
                     if (child.className.includes('workshopaudio')) {
                         child.classList.replace('flex','hidden');
@@ -31,7 +17,7 @@ const displaySelected = event => {
             }
         }
         //set the selected workshop to display as selected
-        selectedWorkshop.classList.replace('border-pink-50','border-pink-300');
+        selectedWorkshop.classList.replace('border-amber-50','border-amber-200');
         for (const child of selectedWorkshop.children) {
             if (child.className.includes('workshopaudio')) {
                 child.classList.replace('hidden','flex');
